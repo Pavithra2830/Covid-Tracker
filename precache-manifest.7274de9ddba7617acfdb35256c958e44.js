00a31cb2acae6aa92b716559523e071be2b1bde0
Pavithra <pavithrav17065@it.ssn.edu.in>
2020-08-13 22:45:30 +0530

    Updates

---
+++ precache-manifest.7274de9ddba7617acfdb35256c958e44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070a6098e4863fc7bdbeab84b24b13ee",
+    "url": "/Covid-Tracker.git/index.html"
+  },
+  {
+    "revision": "d520af24bd7a9416124c",
+    "url": "/Covid-Tracker.git/static/css/main.b8cfd575.chunk.css"
+  },
+  {
+    "revision": "b4d516dd7976ec88b34b",
+    "url": "/Covid-Tracker.git/static/js/2.31b4f1e4.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/Covid-Tracker.git/static/js/2.31b4f1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d520af24bd7a9416124c",
+    "url": "/Covid-Tracker.git/static/js/main.53fbf044.chunk.js"
+  },
+  {
+    "revision": "83fb21836dcc83fc0051",
+    "url": "/Covid-Tracker.git/static/js/runtime-main.f7991e86.js"
+  },
+  {
+    "revision": "fd59fe9ea4258192839496ed65af7086",
+    "url": "/Covid-Tracker.git/static/media/image.fd59fe9e.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid-Tracker.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
